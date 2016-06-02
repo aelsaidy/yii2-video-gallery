6d5a61e589572e1a18218d8af0996ff1203764bb
aelsaidy <ahmad.elsaidy@hotmail.com>
2016-06-02 17:16:07 +0300

    Update update.php

--- views/admin/video/update.php
+++ views/admin/video/update.php
@@ -2,6 +2,11 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use common\models\Tag;
+use kartik\select2\Select2;
+use yii\helpers\ArrayHelper;
+use yii\web\JsExpression;
+
 
 /**
  * @var yii\web\View $this
@@ -34,6 +39,35 @@ $this->params['breadcrumbs'][] = $this->title;
         <?php $form = ActiveForm::begin(); ?>
 
         <?= $form->field($model, 'url')->textInput(['maxlength' => 255, 'autofocus' => true]) ?>
+        
+        <?php 
+
+        // The controller action that will render the list
+        $url = \yii\helpers\Url::to(['/tag/taglist']);
+
+        // Get the initial city description
+        $cityDesc = empty($model->key_words) ? '' : Tag::findOne($model->key_words)->name;
+
+        echo $form->field($model, 'key_words')->widget(Select2::classname(), [
+            'initValueText' => $cityDesc, // set the initial display text
+            'options' => ['placeholder' => 'ابحث عن كلمة مفتاحية ...'],
+            'pluginOptions' => [
+                'allowClear' => true,
+                'minimumInputLength' => 3,
+                'language' => [
+                    'errorLoading' => new JsExpression("function () { return 'Waiting for results...'; }"),
+                ],
+                'ajax' => [
+                    'url' => $url,
+                    'dataType' => 'json',
+                    'data' => new JsExpression('function(params) { return {q:params.term}; }')
+                ],
+                'escapeMarkup' => new JsExpression('function (markup) { return markup; }'),
+                'templateResult' => new JsExpression('function(city) { return city.text; }'),
+                'templateSelection' => new JsExpression('function (city) { return city.text; }'),
+            ],
+        ]);
+        //echo $form->field($model, 'key_words')->textInput(['maxlength' => 255]) ?>
 
         <?= $form->field($model, 'title')->textInput(['maxlength' => 255]) ?>
 
